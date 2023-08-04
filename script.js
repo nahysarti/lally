cb077d7f28d5bb57fd2a60d8c81a2489e82d3e12
nahysarti <nahysarti@gmail.com>
2023-08-04 17:52:43 -0300

    atualização

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ window.addEventListener("DOMContentLoaded", function () {
     firstItem.classList.toggle("tremble-animation")
   }, 1000)
 })
+
+// Função para mostrar os itens ocultos
+function showHiddenItems() {
+  const existingItems = document.querySelectorAll("ul li.seesaw-item")
+  existingItems.forEach((item) => {
+    item.style.display = "list-item"
+  })
+
+  // Ocultar o botão após clicar nele
+  const showButton = document.getElementById("show-hidden-items")
+  showButton.style.display = "none"
+}
+
+// Evento de clique no botão para mostrar os itens ocultos
+const showButton = document.getElementById("show-hidden-items")
+showButton.addEventListener("click", showHiddenItems)
